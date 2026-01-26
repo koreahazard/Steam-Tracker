5c922d456a6ca9a356c601d97a799c5a560212b5
이형준 <gggudwnsgg@naver.com>
2026-01-26 18:57:23 +0900

    init: steam-tracker 레포지토리 생성

---
+++ src/test/java/com/example/steam_tracker/SteamTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.steam_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
