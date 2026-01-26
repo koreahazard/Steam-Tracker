deb318dd4ed7b4a67c505668689c6de44aa0f882
이형준 <gggudwnsgg@naver.com>
2026-01-26 18:48:22 +0900

    init: steam-tracker 레포지토리 생성

---
+++ src/main/java/com/example/steam_tracker/SteamTrackerApplication.java
@@ -0,0 +1,14 @@
+package com.example.steam_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamTrackerApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
