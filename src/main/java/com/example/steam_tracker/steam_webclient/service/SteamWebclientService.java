8e665695497ec49b539746f7da69b5aebb96c1fe
koreahazard <gggudwnsgg@naver.com>
2026-02-10 22:23:44 +0900

    wip: steamWebclientService 구현 중

---
+++ src/main/java/com/example/steam_tracker/steam_webclient/service/SteamWebclientService.java
@@ -0,0 +1,7 @@
+package com.example.steam_tracker.steam_webclient.service;
+
+import java.util.Set;
+
+public interface SteamWebclientService {
+    getGameDetailByAppId(Set<Integer> set)
+}
---
+++ src/main/java/com/example/steam_tracker/steam_webclient/service/SteamWebclientServiceImpl.java
@@ -0,0 +1,19 @@
+package com.example.steam_tracker.steam_webclient.service;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Slf4j
+@Service
+@Getter
+@RequiredArgsConstructor
+public class SteamWebclientServiceImpl {
+
+    getGameDetailByAppId(Set<Integer> set) {
+
+    }
+}
