2a48a30b6726fd69a53221696c7763dd9faf2c74
이형준 <gggudwnsgg@naver.com>
2026-05-20 20:54:45 +0900

    chore: vercel 프론드엔드 주소 재수정

--- src/main/java/com/example/steam_tracker/config/GlobalCorsConfig.java
+++ src/main/java/com/example/steam_tracker/config/GlobalCorsConfig.java
@@ -11,7 +11,7 @@ public class GlobalCorsConfig implements WebMvcConfigurer {
 		registry.addMapping("/**")
 				.allowedOrigins(
 						"http://localhost:5173",
-						"https://copy-react-steam-tracker.vercel.app"
+						"https://react-steam-tracker-front.vercel.app/"
 				)
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 				.allowedHeaders("*")
